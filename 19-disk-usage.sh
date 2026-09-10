1d7d50707af5dd5cbbfe7fb888345546b886323a
Manisha <manishamaidam169@gamil.com>
2026-09-10 15:54:41 +0530

    diskusage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -16,6 +16,6 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $MESSAGE"
 
-echo "$MESSAGE" | mail -s "Disk Usage Alert" maidammanisha.com
+echo "$MESSAGE" | mail -s "Disk Usage Alert" maidammanisha@gmail.com
 
 # echo "body" | mail -s "subject" to-address
