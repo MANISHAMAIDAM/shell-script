10e6d2e3de947b7d556b917bb7b3a83c580b6711
Manisha <manishamaidam169@gamil.com>
2026-09-10 14:22:51 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Ishvik, How are you?"
+echo "Ishvik:: Hi Papa, I am fine. How are you?"
+echo "Ramesh: I am fine too. how is your progressing?"
+echo "Ishvik:: not bad. I am thinking to upgrade to crawling"
